d4e5190c0eede31e26376889a3cc06ffd36e9260
Dennis <dennimanadams@gmail.com>
2020-11-03 22:29:45 +0100

    add location storage to save theme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 const checkbox = document.querySelector(".checkbox")
+let getDarkMode = localStorage.getItem('darkMode')
+
+const enableDarkMode = () => {
+    document.body.classList.add('darktheme-bg')
+    localStorage.setItem('darkMode', 'enabled')
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('darktheme-bg')
+    localStorage.setItem('darkMode', null)
+}
+
+if(getDarkMode === 'enabled') {
+    enableDarkMode()
+}
+
+
+
+
+
 
 checkbox.addEventListener('change', () => {
-    document.body.classList.toggle('darktheme-bg')
+    getDarkMode = localStorage.getItem('darkMode')
+
+    if(getDarkMode !== 'enabled') {
+        enableDarkMode() 
+    } else {
+        disableDarkMode()
+    }
 })
+
+
+
